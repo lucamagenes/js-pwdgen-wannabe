72fbc0f51d39c032fc4dcdf5784d7ddf8372190e
lucamagenes <lucamgns33@gmail.com>
2021-10-11 11:21:47 +0200

    aggiunta della variabile anno corrente

--- main.js
+++ main.js
@@ -10,11 +10,12 @@ const user_last_name = prompt("Qual'è il tuo cognome?");
 const user_favourite_color = prompt("Qual'è il tuo colore preferito?");
 
 
+const current_year = 21;
 
 //console
 console.log(user_name + user_last_name + user_favourite_color);
 
-
+const password = user_name + user_last_name + user_favourite_color + current_year;
 
 //scrivi a schermo i dati dell'utente
-document.getElementById("password").innerHTML = user_name + user_last_name + user_favourite_color + "21"
+document.getElementById("password").innerHTML = password
